7b0acaf3d8c80651728d67981f3535be6ebc64ed
MarceloISR <154558533+MarceloISR@users.noreply.github.com>
2024-02-27 14:27:03 -0400

    Update backEnd.ts

--- src/utils/backEnd.ts
+++ src/utils/backEnd.ts
@@ -14,7 +14,6 @@ export interface InventoryData {
 
 
 export interface iItemStock {
-    //itemId: 15557,
     itemCode: string,
     itemDescription: string,
     lotCode: string,
@@ -26,7 +25,6 @@ export interface iItemStock {
     qtyHold: number,
     qtyQuarantine: number,
     totalQty: number,
-    //vendorId: 16909,
     vendorDescription: string,
     uomAbbrev: string,
     pieceCount: number,
@@ -34,13 +32,11 @@ export interface iItemStock {
 
 export interface iItemDetail {
     
-    //warehouseId: number,
-    //itemId: number,
+
     itemCode: string,
     itemDescription: string,
     lotCode: string,
     sublotCode: string,
-    //storageId: number,
     storageLocation: string,
     lpn: string,
     available: number,
@@ -49,22 +45,16 @@ export interface iItemDetail {
     quarantine: number,
     allocated: number,
     planned: number,
-    //vendorId: number,
-    //vendorDescription: string,
     lbs: number,
     uomAbbrev: string,
-    //itemType: number,
     level: string,
     bay: string,
     slot: string,
-    //category: number,
     velocity0: number,
     velocityA: number,
     velocityB: number,
     velocityC: number,
     velocityD: number,
-    //valuePerEach: number,
-    //itemVelocity: number,
     zone: string,
     locationStatus: string,
 }
@@ -86,19 +76,7 @@ export class backEnd {
     }
     
     public async LoadScenario(args: scenario): Promise<void> {
-        
-        // const credentialGroup : string | undefined =  args.CredentialGroup ?? args?.Scenario?.CredentialGroup; 
-        // SetUICredentials(credentialGroup, args?.Credentials?.UI);
-        // SetRFCredentials(credentialGroup, args?.Credentials?.RF);
-        // SetAPICredentials(credentialGroup, args?.Credentials?.API);
-        
-        // RF.warehouse = args?.Scenario?.warehouse?.description ?? RF.warehouse;
-        // API.warehouse = args?.Scenario?.warehouse?.description ?? API.warehouse;
-        // WEB.warehouse = args?.Scenario?.warehouse?.description ?? WEB.warehouse;
-
-        // const aux = await SelectDatabaseByText('bstevens');
-        // const warehouse = await SelectWarehouseByText('Automation');
-
+    
         if(args){
             const APICall = InitAPI();
 
@@ -134,28 +112,5 @@ export class backEnd {
         
         return bpResp.length>0;
     }
-    // public async LoadScenario(args: scenario): Promise<void> {
-    //     if(args){
-    //         const APICall = InitAPI();
-
-    //         const whResp = await APICall.getWarehouse(args);
-    //         if(!whResp) throw new Error(`Error: the warehouse ${args?.warehouse?.description} was not correcly udpated/saved`);
-
-    //         const bpResp = await APICall.getVendors(args);
-    //         if(args.businessPartners && bpResp.length != args.businessPartners.length) throw new Error(`Error: not all the accounts were created/updated`);
-
-    //         const sLocationResponse = await APICall.getStorageLocations(args, whResp.id!);
-    //         if(args.storageLocations && sLocationResponse.length != args.storageLocations?.Locations?.length) throw new Error(`Error: not all the Storage Locations were created/updated`);
-
-    //         const itemsResponse =  await APICall.getItems(args); 
-    //         if(args.items && itemsResponse.length != args.items?.length) throw new Error(`Error: not all the items were created/updated`);
-
-    //         const invAdjResponse = await APICall.getInventoryAdjustment(args, whResp?.id!);
-    //         if(!invAdjResponse) throw new Error(`Error: not all the inventory adjustment were created/updated`);
-
-    //         const seqResponse = await APICall.getSequences(args);
-    //         if(args.sequences && seqResponse.length != args.sequences?.length) throw new Error(`Error: not all the sequences were saved`);
-    //     }
-    // }
-
+   
 }
